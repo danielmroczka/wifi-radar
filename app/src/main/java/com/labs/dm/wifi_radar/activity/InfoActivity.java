056604e46abf981996202a755cda0a7868586938
daniel <daniel.mroczka@gmail.com>
2015-07-09 02:53:51 +0200

    Refactoring

--- app/src/main/java/com/labs/dm/wifi_radar/activity/InfoActivity.java
+++ app/src/main/java/com/labs/dm/wifi_radar/activity/InfoActivity.java
@@ -1,9 +1,12 @@
 package com.labs.dm.wifi_radar.activity;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.labs.dm.wifi_radar.R;
 
@@ -13,12 +16,21 @@ public class InfoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        PackageInfo pInfo = null;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("WIFI Radar ver." + pInfo.versionName);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_info, menu);
-        return true;
+        return false;
     }
 
     @Override
